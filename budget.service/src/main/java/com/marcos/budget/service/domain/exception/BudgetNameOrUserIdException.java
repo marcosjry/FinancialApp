d11588016d98b0ee9dff9844fb794e58c2eb9114
Marcos Antônio <marcosjunior-me@ufu.br>
2025-01-28 19:21:22 -0300

    feat: adicionado exceções customizadas para tratamento de erros

---
+++ budget.service/src/main/java/com/marcos/budget/service/domain/exception/BudgetNameOrUserIdException.java
@@ -0,0 +1,7 @@
+package com.marcos.budget.service.domain.exception;
+
+public class BudgetNameOrUserIdException extends Exception {
+    public BudgetNameOrUserIdException(String message) {
+        super(message);
+    }
+}
