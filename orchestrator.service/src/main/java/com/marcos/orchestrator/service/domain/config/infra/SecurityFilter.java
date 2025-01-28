b0eaca0116e61a9b0d34b41393a24e60a5496595
Marcos Antônio <marcosjunior-me@ufu.br>
2025-01-28 15:22:39 -0300

    feat: adicionado validação de token jwt e criado Bean para injetar as secrets

---
+++ orchestrator.service/src/main/java/com/marcos/orchestrator/service/domain/config/infra/SecurityFilter.java
@@ -0,0 +1,54 @@
+package com.marcos.orchestrator.service.domain.config.infra;
+
+import com.marcos.orchestrator.service.domain.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        System.out.println(token);
+        if(token != null) {
+            var role = tokenService.extractClaim(token, "role");
+            if(role.equals("ROLE_SERVICE")) {
+                var userId = this.tokenService.validateServiceToken(token);
+                var authentication = new UsernamePasswordAuthenticationToken(userId, null, List.of(new SimpleGrantedAuthority(role)));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            if(role.equals("ROLE_USER")) {
+                var userId = this.tokenService.validateToken(token);
+                var authentication = new UsernamePasswordAuthenticationToken(userId, null, List.of(new SimpleGrantedAuthority(role)));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+
+}
